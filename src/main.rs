576bb5ba915ac522805589ddce5d65141e683f28
Martin Kooij <martink.kooij.4@gmail.com>
2024-10-17 21:50:12 +0200

    Soring half-way there

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 pub mod sensor_reading;
 
-use actix_web::{cookie, HttpServer, App, web, HttpResponse, Responder};
-// use actix_files::Files;
+use actix_web::{HttpServer, App, web, HttpResponse, Responder};
+use actix_files::Files;
 // use actix_web::middleware::Logger;
 // use actix_rt;
-use std::sync::Mutex;
+
 
 use tera::{Tera, Context};
 use lazy_static::lazy_static;
-use sensor_readings::{read_temp, read_humidity};
+use sensor_reading::{read_temp, read_humidity};
 
 
 lazy_static!{
@@ -34,18 +34,17 @@ async fn index(tera: web::Data<Tera>) -> impl Responder {
 }
 
 
-#[tokio::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
     HttpServer::new ( move || {
         App::new()
             .service(Files::new("/assets", "./assets").prefer_utf8(true))
             .app_data(web::Data::new(TEMPLATES.clone()))
-            .app_data( robot_address.clone()) 
             .route("/",web::get().to(index))
             .route("/gettemperature", web::get().to(read_temp))
             .route("/gethumidity",web::get().to(read_humidity))
-    })
+     })
     .bind(("127.0.0.1", 8000))?
     .run()
     .await
